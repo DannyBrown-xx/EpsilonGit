a67011f7515c473b23d50bc70bd82c93cfbc686f
Daniel Brown <d.t.brown@outlook.com>
2015-07-01 22:37:38 +0100

    Added method to get all committed commits by a committer, similar to getting authored commits by an author

--- Implementation/org.eclipse.epsilon.emc.git/src/org/eclipse/epsilon/emc/git/people/Committer.java
+++ Implementation/org.eclipse.epsilon.emc.git/src/org/eclipse/epsilon/emc/git/people/Committer.java
@@ -1,10 +1,33 @@
 package org.eclipse.epsilon.emc.git.people;
 
+import java.util.Collection;
+import java.util.LinkedList;
+
 import org.eclipse.epsilon.emc.git.GitModel;
+import org.eclipse.epsilon.emc.git.objectmodel.Commit;
+import org.eclipse.epsilon.eol.exceptions.models.EolModelElementTypeNotFoundException;
 
 public class Committer extends Person {
 
 	public Committer(String name, String emailAddress, GitModel owner) {
 		super(name, emailAddress, owner);
+	}
+	
+	public Collection<Commit> getAllCommittedCommits() {
+		try {
+			@SuppressWarnings("unchecked")
+			LinkedList<Commit> allCommits = (LinkedList<Commit>) this.owner.getAllOfType("Commit");
+			Collection<Commit> thisCommittersCommits = new LinkedList<Commit>();
+			for(Commit commit : allCommits) {
+				if(commit.getCommitterIdent().getEmailAddress().equals(this.getEmailAddress()) &&
+					commit.getCommitterIdent().getName().equals(this.getName())) {
+					thisCommittersCommits.add(commit);
+				}
+			}
+			return thisCommittersCommits;
+		} 
+		catch (EolModelElementTypeNotFoundException e) {
+			return null;
+		}
 	}
 }
